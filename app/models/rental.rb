58b0695acca7ede900e4298477a5f00df4297016
Raphael Jube <raphaeljube@ymail.com>
2022-05-25 12:53:46 +0200

    Updates on rentals controller

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,29 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :duck
-  validates :date_start, presence: true #, comparison: { greater_than: :end_date }
-  validates :date_end, presence: true #, comparison: { less_than: :end_date }
+  validates :date_start, :date_end, presence: true
+  validate :date_end_after_date_start
+  validate :rental_available?, on: :create
+
+  private
+
+  def rental_available?
+    @rentals = Rental.all
+    @rentals.each do |rental|
+      if rental.date_end >= date_start && rental.date_start <= date_start
+        errors.add(:date_start, "Sorry, not available")
+      end
+      if rental.date_end >= date_end && rental.date_start <= date_end
+        errors.add(:date_end, "Sorry, not available")
+      end
+    end
+  end
+
+  def date_end_after_date_start
+    return if date_end.blank? || date_start.blank?
+
+    if date_end < date_start
+      errors.add(:date_end, "must be after the start date")
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+skills = ["CSS", "HTML", "Javascript", "Ruby", "Python"]
+skills.each do |skill|
+  s = Skill.new
+  s.name = skill
+  s.save
+end
