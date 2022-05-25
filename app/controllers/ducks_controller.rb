c27553cdc50bb2bb667444dd6497e2bd8232a0a6
PaulHenri Leblanc <paulhenri.leblanc@gmail.com>
2022-05-25 12:06:58 +0200

    working Create for Ducks

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -13,7 +13,11 @@ class DucksController < ApplicationController
 
   def create
     @duck = Duck.new(duck_params)
-    # @duck.skills =
+    @user = User.find_by(id: params[:duck][:users])
+    @duck.user = @user
+    @skills = params[:duck][:skills]
+    @skills.shift
+    @duck.skills = @skills.map { |skill| Skill.find_by(id: skill) }
     if @duck.save
       redirect_to duck_path(@duck)
     else
@@ -24,6 +28,6 @@ class DucksController < ApplicationController
   private
 
   def duck_params
-    params.require(:duck).permit(:name, :users, :description, :price)
+    params.require(:duck).permit(:name, :description, :price)
   end
 end
